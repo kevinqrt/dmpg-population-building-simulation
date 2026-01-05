efd5d5998ae8f28e9b60e72337ec021fc3ea614d
kevinqrt <kevin.brungard@node.energy>
2026-01-06 00:06:39 +0100

    add fixed simulation results for lingen

--- examples/dmpg/population_building_simulation/config.py
+++ examples/dmpg/population_building_simulation/config.py
@@ -2,12 +2,14 @@
 This module contains configuration constants for the population building simulation.
 """
 
-from examples.dmpg.population_building_simulation.helpers import get_osm_population_from_api_or_cached_file
+from examples.dmpg.population_building_simulation.helpers import (
+    get_osm_population_from_api_or_cached_file,
+)
 
 
-CITY_NAME = "Lingen (Ems)" # other examples: Lingen (Ems), "Hamburg", "Berlin"
+CITY_NAME = "Lingen (Ems)"  # other examples: Lingen (Ems), "Hamburg", "Berlin"
 CITY_POPULATION = get_osm_population_from_api_or_cached_file(CITY_NAME)
-SIMULATION_DAYS = 2
+SIMULATION_DAYS = 30
 
 # Work schedule: 6am - 6pm
 WORK_START_HOUR = 6
@@ -17,7 +19,7 @@ WORK_END_HOUR = 18
 WORK_DURATION_MIN = 420  # 7 hours in minutes
 WORK_DURATION_MAX = 540  # 9 hours in minutes
 
-#https://www.wegweiser-kommune.de/data-api/rest/report/export/demografiebericht+lingen-ems.pdf
+# https://www.wegweiser-kommune.de/data-api/rest/report/export/demografiebericht+lingen-ems.pdf
 BIRTHS_PER_DAY = 1.5
 DEATHS_PER_DAY = 1.75
 
